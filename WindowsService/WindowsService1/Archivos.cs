73c03ba34f3ea5af5b9ac672cafb809103798025
afa001 <anderson.alarcon.afa@gmail.com>
2020-11-05 12:14:20 -0500

    Se elimino el archivo por defecto Services1, y se agrego el servicio Archivos

--- WindowsService/WindowsService1/Archivos.cs
+++ WindowsService/WindowsService1/Archivos.cs
@@ -10,19 +10,21 @@ using System.Threading.Tasks;
 
 namespace WindowsService1
 {
-    public partial class Service1 : ServiceBase
+    partial class Archivos : ServiceBase
     {
-        public Service1()
+        public Archivos()
         {
             InitializeComponent();
         }
 
         protected override void OnStart(string[] args)
         {
+            // TODO: agregar código aquí para iniciar el servicio.
         }
 
         protected override void OnStop()
         {
+            // TODO: agregar código aquí para realizar cualquier anulación necesaria para detener el servicio.
         }
     }
 }
--- WindowsService/WindowsService1/Program.cs
+++ WindowsService/WindowsService1/Program.cs
@@ -17,7 +17,7 @@ namespace WindowsService1
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
             {
-                new Service1()
+                new Archivos() //Al iniciar se ejecuta el servicio de Archivos
             };
             ServiceBase.Run(ServicesToRun);
         }
